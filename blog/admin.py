ed24261155b84e8a351af47eb5857bd08f739b08
MiaRasmussen05 <rasmussenm376@gmail.com>
2023-04-26 18:44:35 +0000

    Build on the admin site and install summernote

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import ShopCategory, Shop, Review
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(ShopCategory)
+class ShopCategoryAdmin(SummernoteModelAdmin):
+
+    search_fields = ['name']
+
+
+@admin.register(Shop)
+class ShopAdmin(SummernoteModelAdmin):
+
+    list_filter = ('status', 'created_on', 'item', 'price')
+    list_display = ('title', 'slug', 'status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
+    search_fields = ['title', 'description', 'price', 'item']
+    summernote_fields = ('description')
+
+
+@admin.register(Review)
+class ReviewAdmin(SummernoteModelAdmin):
+
+    list_filter = ('created_on', 'text', 'comment', 'user', 'rating')
+    list_display = ('user', 'rating', 'text', 'comment', 'created_on')
+    search_fields = ['user', 'rating', 'text']
--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['portfolio-project4.herokuapp.com']
+ALLOWED_HOSTS = ['portfolio-project4.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'django_summernote',
     'blog',
 ]
 
