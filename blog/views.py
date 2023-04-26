96237850a51ad75ae0f40fe6a7ed11b7fad00b55
MiaRasmussen05 <rasmussenm376@gmail.com>
2023-04-26 21:01:34 +0000

    Add the shop views and the start templets

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Shop
 
-# Create your views here.
+
+def shop_category(request, shop_category_id):
+    categories = ShopCategory.objects.all()
+    selected_category = get_object_or_404(ShopCategory, id=shop_category_id)
+    items = Shop.objects.filter(shop_category=selected_category)
+    context = {'categories': categories, 'items': items, 'selected_category': selected_category}
+
+    return render(request, 'shop.html', context)
+
+
+def shop(request):
+    categories = ShopCategory.objects.all()
+    queryset = Shop.objects.filter(status=1)
+    selected_category_id = request.GET.get('category')
+    if selected_category_id:
+        selected_category = get_object_or_404(ShopCategory, id=selected_category_id)
+        shop_items = queryset.filter(shop_category=selected_category)
+    else:
+        selected_category = None
+        shop_items = queryset.all()
+
+    return render(request, 'shop.html', {'categories': categories, 'selected_category': selected_category, 'shop_items': shop_items})
