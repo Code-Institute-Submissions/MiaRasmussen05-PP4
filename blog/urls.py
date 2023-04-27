2923d923a671002f5f6a96fdbce22621d25964ce
MiaRasmussen05 <rasmussenm376@gmail.com>
2023-04-27 19:33:59 +0000

    Link up shop page and redo the shop and category views from functions into class based views

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+
+    path('shop/', views.ShopView.as_view(), name='shop'),
+    path('shop/<int:shop_category_id>/', views.ShopCategoryView.as_view(), name='shop_category'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,30 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
-from .models import Shop
+from django.views.generic import ListView
+from .models import Shop, ShopCategory
 
 
 def home(request):
     return render(request, 'index.html')
 
 
-def shop_category(request, shop_category_id):
-    categories = ShopCategory.objects.all()
-    selected_category = get_object_or_404(ShopCategory, id=shop_category_id)
-    items = Shop.objects.filter(shop_category=selected_category)
-    context = {'categories': categories, 'items': items, 'selected_category': selected_category}
+class ShopCategoryView(ListView):
+    template_name = 'shop.html'
+    context_object_name = 'items'
+    model = ShopCategory
 
-    return render(request, 'shop.html', context)
+    def get_queryset(self):
+        selected_category = get_object_or_404(ShopCategory, id=self.kwargs['shop_category_id'])
+        queryset = super().get_queryset().filter(shop_category=selected_category)
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        categories = ShopCategory.objects.all()
+        selected_category = get_object_or_404(ShopCategory, id=self.kwargs['shop_category_id'])
+        context.update({'categories': categories, 'selected_category': selected_category})
+        return context
 
 
-def shop(request):
-    categories = ShopCategory.objects.all()
-    queryset = Shop.objects.filter(status=1)
-    selected_category_id = request.GET.get('category')
-    if selected_category_id:
-        selected_category = get_object_or_404(ShopCategory, id=selected_category_id)
-        shop_items = queryset.filter(shop_category=selected_category)
-    else:
-        selected_category = None
-        shop_items = queryset.all()
+class ShopView(ListView):
+    template_name = 'shop.html'
+    context_object_name = 'shop_items'
+    model = Shop
 
-    return render(request, 'shop.html', {'categories': categories, 'selected_category': selected_category, 'shop_items': shop_items})
+    def get_queryset(self):
+        queryset = super().get_queryset().filter(status=1)
+        selected_category_id = self.request.GET.get('category')
+        if selected_category_id:
+            selected_category = get_object_or_404(ShopCategory, id=selected_category_id)
+            queryset = queryset.filter(shop_category=selected_category)
+            self.selected_category = selected_category
+        else:
+            self.selected_category = None
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        categories = ShopCategory.objects.all()
+        context.update({'categories': categories, 'selected_category': self.selected_category})
+        return context
