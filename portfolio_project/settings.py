6210c725764e59011fff1b726034ae6fc471c2ed
MiaRasmussen05 <rasmussenm376@gmail.com>
2023-04-27 00:53:16 +0000

    Install alluth for registration and add some more style to the navbar

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -41,6 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'django.contrib.messages',
     'cloudinary_storage',
     'django.contrib.staticfiles',
@@ -48,6 +52,11 @@ INSTALLED_APPS = [
     'django_summernote',
     'blog',
 ]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
