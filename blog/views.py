3319b78e29c8a43768c99e817128b5f8686e2c64
MiaRasmussen05 <rasmussenm376@gmail.com>
2023-04-28 23:55:02 +0000

    Start on view for blog details

--- blog/views.py
+++ blog/views.py
@@ -129,3 +129,46 @@ class BlogList(generic.ListView):
     queryset = Blog.objects.filter(status=1).order_by('-created_on')
     template_name = 'blog.html'
     paginate_by = 12
+
+
+class BlogDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Blog.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.all().order_by('created_on')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "blog_post.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": False,
+                "liked": liked,
+                "comment_form": CommentForm(),
+            },
+        )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Blog.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.all().order_by('created_on')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "blog_post.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": False,
+                "liked": liked,
+                "comment_form": comment_form,
+            },
+        )
