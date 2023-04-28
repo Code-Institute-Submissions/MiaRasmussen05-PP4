0c2858044f7c470229b96db92dd956328e2c9e39
MiaRasmussen05 <rasmussenm376@gmail.com>
2023-04-28 22:01:32 +0000

    Create profile page html

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('profile/', views.ProfileDetailView.as_view(), name='profile'),
+    path('<username>/', views.ProfileDetailView.as_view(), name='profile'),
     path('create_profile/', views.CreateProfileView.as_view(), name='create_profile'),
 ]
