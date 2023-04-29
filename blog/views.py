feab852e2dc5e52a30edb54cb75133615277d9fb
MiaRasmussen05 <rasmussenm376@gmail.com>
2023-04-29 01:44:04 +0000

    Add and style comment section in blog post

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, HttpResponseRedirect
 from django.views import generic, View
 from django.views.generic import ListView, DetailView, CreateView
 from django.contrib import messages
