fdb87cc75f5fff46830298bb59b75ae215458162
MiaRasmussen05 <rasmussenm376@gmail.com>
2023-05-17 20:57:25 +0000

    Add first part of models test cases

--- blog/tests/test_models.py
+++ blog/tests/test_models.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.utils.text import slugify
+from django.core.validators import MinLengthValidator
+from decimal import Decimal
 from blog.models import (
     ShopCategory, Shop, Review, Blog, Comment, Projects, Images)
 from blog.views import GalleryCategory
@@ -59,7 +61,8 @@ class TestModels(TestCase):
             status=1
         )
 
-        self.gallery_category = GalleryCategory.objects.create(name='Category 1')
+        self.gallery_category = GalleryCategory.objects.create(
+            name='Category 1')
 
         self.image = Images.objects.create(
             image='image.jpg',
@@ -69,3 +72,47 @@ class TestModels(TestCase):
             category=self.gallery_category,
             status=1
         )
+
+    def test_shop_category_str(self):
+        """
+        Tests the string representation of the ShopCategory model
+        """
+        shop_category = ShopCategory.objects.get(name='Category 1')
+        self.assertEqual(str(shop_category), 'Category 1')
+
+    def test_shop_str(self):
+        """
+        Tests the string representation of the Shop model
+        """
+        shop = Shop.objects.get(title='Shop 1')
+        expected_str = "Shop 1 - Item 1: $2.50"
+        self.assertEqual(str(shop), expected_str)
+
+    def test_shop_slug_is_assigned(self):
+        """
+        Tests whether a slug is being assigned to the Shop model
+        """
+        self.assertEqual(self.shop.slug, 'shop-1')
+
+    def test_shop_save_method(self):
+        """
+        Tests the custom save method of the Shop model
+        """
+        new_shop = Shop.objects.create(
+            title='New Shop',
+            item='New Item',
+            price=Decimal('5.00'),
+            shop_category=self.shop_category,
+            status=1
+        )
+        self.assertEqual(new_shop.slug, 'new-shop')
+
+    def test_shop_price_validation(self):
+        """
+        Tests the price validation in the clean method of the Shop model
+        """
+        invalid_shop = Shop(
+            title='Invalid Shop',
+            price=-10.0,
+        )
+        self.assertEqual(invalid_shop.price, -10.0)
