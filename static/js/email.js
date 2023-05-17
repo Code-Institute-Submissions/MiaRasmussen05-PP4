7bb6b7d5cd06747805eeabbda29a73aa912f431c
MiaRasmussen05 <rasmussenm376@gmail.com>
2023-05-17 13:25:20 +0000

    Set up contact form

---
+++ static/js/email.js
@@ -0,0 +1,22 @@
+function sendMail(contactForm) {
+    emailjs.send("service_020r79k", "template_6nngexk", {
+        "to_name": "Mia Rasmussen",
+        "from_fiName": contactForm.fiName.value,
+        "from_laName": contactForm.laName.value,
+        "from_email": contactForm.email.value,
+        "for_reason": contactForm.reason.value,
+        "inquiry_request": contactForm.teArea.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    contactForm.preventDefault();
+    contactForm.target.reset();
+    
+    return false;
+}
