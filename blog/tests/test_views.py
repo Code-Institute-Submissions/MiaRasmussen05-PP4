f42158dba2d7a65c4da749a9a6f9b47d1a972fe8
MiaRasmussen05 <rasmussenm376@gmail.com>
2023-05-19 07:56:31 +0000

    Add third part of views test cases

--- blog/tests/test_views.py
+++ blog/tests/test_views.py
@@ -2,7 +2,8 @@ from django.test import TestCase, Client
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 from blog.models import (
-    ShopCategory, Shop, Review, Blog, Comment, Projects, Images)
+    ShopCategory, Shop, Review, Blog,
+    Comment, Projects, GalleryCategory, Images)
 
 
 class TestViews(TestCase):
@@ -33,6 +34,20 @@ class TestViews(TestCase):
         self.name = 'Jane Doe'
         self.email = 'janedoe@example.com'
         self.comment = 'Testing comment!'
+
+        self.project1 = Projects.objects.create(title='Project 1', status=1)
+        self.project2 = Projects.objects.create(title='Project 2', status=1)
+        self.project3 = Projects.objects.create(title='Project 3', status=0)
+
+        self.category1 = GalleryCategory.objects.create(name='Category 1')
+        self.category2 = GalleryCategory.objects.create(name='Category 2')
+
+        self.image1 = Images.objects.create(
+            title='Image 1', category=self.category1)
+        self.image2 = Images.objects.create(
+            title='Image 2', category=self.category1)
+        self.image3 = Images.objects.create(
+            title='Image 3', category=self.category2)
 
     def test_home_view(self):
         """
@@ -125,3 +140,57 @@ class TestViews(TestCase):
             'comment': self.comment,
         })
         self.assertEqual(response.status_code, 200)
+
+    def test_project_list_view(self):
+        """
+        Test the ProjectList view for listing projects
+        """
+        url = reverse('portfolio')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'portfolio.html')
+
+        self.assertIn(self.project1, response.context['object_list'])
+        self.assertIn(self.project2, response.context['object_list'])
+        self.assertNotIn(self.project3, response.context['object_list'])
+
+        projects = response.context['object_list']
+        self.assertEqual(projects[0], self.project2)
+        self.assertEqual(projects[1], self.project1)
+
+        self.assertEqual(response.context['paginator'].count, 2)
+        self.assertEqual(len(projects), 2)
+
+    def test_gallery_view_with_category(self):
+
+        url = reverse('gallery_category', kwargs={
+            'category_id': self.category1.id})
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'gallery.html')
+
+        self.assertEqual(response.context['category'], self.category1)
+        self.assertEqual(list(response.context['images']), [
+            self.image2, self.image1])
+        self.assertEqual(list(response.context['all_images']), [
+            self.image3, self.image2, self.image1])
+        self.assertEqual(list(response.context['categories']), [
+            self.category1, self.category2])
+
+    def test_gallery_view_without_category(self):
+
+        url = reverse('gallery')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'gallery.html')
+
+        self.assertIsNone(response.context['category'])
+        self.assertEqual(list(response.context['images']), [
+            self.image3, self.image2, self.image1])
+        self.assertEqual(list(response.context['all_images']), [
+            self.image3, self.image2, self.image1])
+        self.assertEqual(list(response.context['categories']), [
+            self.category1, self.category2])
